610013d84a3e340971b21174b7ba4dcc60c8c3df
Florian Götting <florian.goetting@fressnapf.com>
2025-04-22 17:40:53 +0200

    Update JSON tag library

--- dist/jsonTagSendData.js
+++ dist/jsonTagSendData.js
@@ -1,20 +1,23 @@
-function jsonTagSendData(url, payload, enableGzip, dataLayerOptions, sendMethod, cleanPayload){
+function jsonTagSendData(url, origPayload, enableGzip, dataLayerOptions, sendMethod, cleanPayload){
     //clean payload
+    let payload = {};
     if(cleanPayload){
-        payload = cleanEventData(payload);
+        payload = cleanEventData(origPayload);
+    } else {
+        payload = origPayload;
     }
-    
+
     const isWebKit = /AppleWebKit/i.test(navigator.userAgent) && !/Chrome|CriOS|OPR|Edg|Edge|FxiOS|SamsungBrowser|Android/i.test(navigator.userAgent); // WebKit has issues with compressionStream :/
-    
+
     let post_headers = {
         'Content-Type': 'application/json'
     };
- 
+
     if( typeof (navigator.sendBeacon) !== 'function' ){
         // in case we do not have sendBeacon, fallback to fetch...
         sendMethod = 'fetch';
     }
-    
+
     if( sendMethod === 'sendBeacon' ){
         navigator.sendBeacon( url, JSON.stringify(payload) );
     } else {
@@ -27,26 +30,26 @@ function jsonTagSendData(url, payload, enableGzip, dataLayerOptions, sendMethod,
             (async function(){
                 // Convert JSON to Stream
                 const stream = new Blob( [JSON.stringify(payload)], {
-                    type: 'application/json',
+                    'type': 'application/json',
                 }).stream();
-            
+
                 // gzip stream
                 const compressedReadableStream = stream.pipeThrough(
                     new CompressionStream('gzip')
                 );
-            
+
                 // create Response
                 const compressedResponse = await new Response(compressedReadableStream);
-            
+
                 // Get response Blob
                 const blob = await compressedResponse.blob();
-            
+
                 // fetch data
                 await fetch( url, {
-                    method: 'POST',
-                    credentials: 'include',
-                    headers: post_headers,
-                    body: blob // send JSON gzipped
+                    'method': 'POST',
+                    'credentials': 'include',
+                    'headers': post_headers,
+                    'body': blob // send JSON gzipped
                 })
                 .then( response => {
                     if( !response.ok ){
@@ -67,10 +70,10 @@ function jsonTagSendData(url, payload, enableGzip, dataLayerOptions, sendMethod,
         } else {  
             //send json uncompressed   
             fetch( url, {
-                method: 'POST',
-                credentials: 'include',
-                headers: post_headers,
-                body: JSON.stringify(payload) // JSON-Body uncompressed!
+                'method': 'POST',
+                'credentials': 'include',
+                'headers': post_headers,
+                'body': JSON.stringify(payload) // JSON-Body uncompressed!
             })
             .then( response => {
                 if( !response.ok ){
@@ -92,28 +95,45 @@ function jsonTagSendData(url, payload, enableGzip, dataLayerOptions, sendMethod,
     function cleanEventData(obj) {
         if (Array.isArray(obj)) {
             return obj
-            .map(cleanEventData)  // Clean each item recursively
-            .filter(item => !(item === null || item === undefined || item === '' || (typeof item === 'object' && Object.keys(item).length === 0)));
-        }
-        else if (typeof obj === 'object' && obj !== null) {
+                .map(cleanEventData)
+                .filter(item =>
+                    item !== null &&
+                    item !== undefined &&
+                    item !== '' &&
+                    !(typeof item === 'object' && Object.keys(item).length === 0) &&
+                    !(typeof item === 'number' && Number.isNaN(item))
+                );
+        } else if (typeof obj === 'object' && obj !== null) {
             return Object.fromEntries(
                 Object.entries(obj)
-                .map(([key, value]) => [key, cleanEventData(value)])  // Clean each property recursively
-                .filter(([, value]) => !(value === null || value === undefined || value === '' || (typeof value === 'object' && Object.keys(value).length === 0)))
+                    .map(([key, value]) => [key, cleanEventData(value)])
+                    .filter(([, value]) =>
+                        value !== null &&
+                        value !== undefined &&
+                        value !== '' &&
+                        !(typeof value === 'object' && Object.keys(value).length === 0) &&
+                        !(typeof value === 'number' && Number.isNaN(value))
+                    )
             );
         }
+    
+        // Primitive case: check if it's NaN
+        if (typeof obj === 'number' && Number.isNaN(obj)) {
+            return undefined; // or null, or just skip it
+        }
+    
         return obj;
-    };
+    }    
     function pushResponseToDataLayer(data, dataLayerOptions) {
         if (dataLayerOptions) {
             const dataLayerName = dataLayerOptions.dataLayerName;
             const dataLayerEventName = dataLayerOptions.dataLayerEventName;
-    
+
             window[dataLayerName] = window[dataLayerName] || [];
-    
-            const eventData = { event: dataLayerEventName };
+
+            const eventData = { 'event': dataLayerEventName };
             const jsonclient = {};
-    
+
             if (data.device_id) {
                 jsonclient.device_id = data.device_id;
             }
@@ -123,11 +143,11 @@ function jsonTagSendData(url, payload, enableGzip, dataLayerOptions, sendMethod,
             if (data.tags) {
                 jsonclient.tags = data.tags;
             }
-    
+
             if (Object.keys(jsonclient).length > 0) {
                 eventData.jsonclient = jsonclient;
             }
-    
+
             if (Object.keys(eventData).length > 1) {
                 eventData._clear = true; 
                 window[dataLayerName].push(eventData);
@@ -135,5 +155,5 @@ function jsonTagSendData(url, payload, enableGzip, dataLayerOptions, sendMethod,
             }
         }
         return false;
-    }    
+    }  
 };
